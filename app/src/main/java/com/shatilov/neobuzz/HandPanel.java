cfb4c17402b5ec836fb68a56743387f00d7de21c
kshatilov <kirill@shatilov.me>
2021-01-07 05:03:06 +0800

    NBTestApp Iteration 3.
    - new HTTPServer and HTTPClient
    - new Hand panel to animate hand position

--- app/src/main/java/com/shatilov/neobuzz/HandPanel.java
+++ app/src/main/java/com/shatilov/neobuzz/HandPanel.java
@@ -2,23 +2,60 @@ package com.shatilov.neobuzz;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 
 import com.shatilov.neobuzz.utils.ColourPalette;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HandPanel extends View {
 
-    private static final int STROKE_WIDTH = 10;
+
+    private static final String TAG = "Hand_Canvas";
 
     private int sizeX, sizeY;
-    private Paint paint;
-    private float SF;
+
+    static final Map<String, Rect> imageRect = new HashMap<>();
+
+    static {
+        imageRect.put("palm", new Rect(0, 0, 647, 569));
+        imageRect.put("thumb", new Rect(0, 0, 294, 417));
+        imageRect.put("thumb_b", new Rect(0, 0, 247, 389));
+        imageRect.put("finger", new Rect(0, 0, 162, 734));
+    }
+
+    private final Drawable palmImage; // 674x667
+    private final Drawable thumbImage; // 294x417
+    private final Drawable fingerImage; // 162x734
+    private final Drawable thumbBImage; // 162x734
+
+    private final double SF = 0.6;
+
+    private float[] pos = {0, 0, 0, 0, 0};
 
     public HandPanel(Context context) {
         super(context);
-        initPaint();
+
+        palmImage = context.getDrawable(R.drawable.palm);
+        thumbImage = context.getDrawable(R.drawable.thumb);
+        thumbBImage = context.getDrawable(R.drawable.thumb_b);
+        fingerImage = context.getDrawable(R.drawable.finger);
+
+    }
+
+    public void setPos(float[] pos) {
+        this.pos = pos;
+        invalidate();
     }
 
     @Override
@@ -26,29 +63,36 @@ public class HandPanel extends View {
         super.onSizeChanged(w, h, oldW, oldH);
         sizeX = w;
         sizeY = h;
-        SF = w / 200.F;
     }
 
-    private void initPaint() {
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(ColourPalette.neuralBlue);
-        paint.setStrokeWidth(STROKE_WIDTH);
+    /**
+     * Pun intended
+     */
+    private Rect getRect(String name) {
+        Rect r = new Rect(imageRect.get(name));
+
+        int left = (int) ((sizeX - r.width() * SF) / 2);
+        int top = (int) ((sizeY - r.height() * SF) / 2);
+        int w = (int) (left + r.width() * SF);
+        int h = (int) (top + r.height() * SF);
+
+        r.set(left, top, w, h);
+        return new Rect(r);
     }
 
+    private Rect placeRect(Rect r, int x, int y) {
+        int w = x + r.width();
+        int h = y + r.height();
+
+        r.set(x, y, w, h);
+
+        return new Rect(r);
+    }
 
     private void drawPalm(Canvas canvas) {
-        Path palm = new Path();
-        palm.moveTo(50 * SF, 100 * SF);
-        palm.lineTo(150 * SF, 100 * SF);
-        palm.lineTo(150 * SF, 180 * SF);
-        palm.lineTo(70 * SF, 180 * SF);
-        palm.lineTo(50 * SF, 120 * SF);
-        palm.close();
-        canvas.drawPath(palm, paint);
+        palmImage.setBounds(getRect("palm"));
+        palmImage.draw(canvas);
     }
-
 
 
     @Override
@@ -60,32 +104,42 @@ public class HandPanel extends View {
     }
 
     private void drawFingers(Canvas canvas) {
-        float startX = 50 * SF;
-        float startY = 1 * SF;
-        float m = 10 * SF;
-        float w = 17 * SF;
-        float h = 95 * SF;
+        int mx = -17;
+        int start = 306;
 
-        for (int i = 0; i < 4; i++) {
-          canvas.drawRect(
-                  startX + ((w + m) * i) + w,
-                  startY + h,
-                  startX + ((w + m) * i),
-                  startY,
-                  paint
-          );
+        int[] dTop = {40, 0, 70, 160};
+        int[] dBottom = {0, 0, 30, 50};
+
+        int[] dBTop = {300, 280, 320, 340};
+        int[] dBBottom = {120, 150, 120, 80};
+
+        Rect original = getRect("finger");
+        int sx = original.width();
+
+        for (int i = 1; i < 5; i++) {
+            Rect r = new Rect(original);
+            r= placeRect(r, start + (i - 1) * (sx + mx), 50);
+
+            r.bottom += (pos[i] == 1) ? dBBottom[i - 1] : dBottom[i - 1];
+            r.top += (pos[i] == 1) ? dBTop[i - 1] : dTop[i - 1];
+
+            fingerImage.setBounds(r);
+            fingerImage.draw(canvas);
         }
     }
 
     private void drawThumb(Canvas canvas) {
-        Path thumb = new Path();
-        thumb.moveTo(20 * SF, 70 * SF);
-        thumb.lineTo(45 * SF,120 * SF);
-        thumb.lineTo(63 * SF,175 * SF);
-        thumb.lineTo(30 * SF,150 * SF);
-        thumb.lineTo(10 * SF,70 * SF);
-        thumb.close();
-        canvas.drawPath(thumb, paint);
+        if (pos[0] == 1) {
+            Rect r = getRect("thumb_b");
+            r = placeRect(r, 175, 365);
+            thumbBImage.setBounds(r);
+            thumbBImage.draw(canvas);
+        } else {
+            Rect r = getRect("thumb");
+            r = placeRect(r, 90, 365);
+            thumbImage.setBounds(r);
+            thumbImage.draw(canvas);
+        }
     }
 
 }
--- app/src/main/java/com/shatilov/neobuzz/MainActivity.java
+++ app/src/main/java/com/shatilov/neobuzz/MainActivity.java
@@ -1,25 +1,36 @@
 package com.shatilov.neobuzz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import com.koushikdutta.async.http.server.AsyncHttpServer;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.shatilov.neobuzz.utils.BuzzAwareActivity;
 import com.shatilov.neobuzz.utils.BuzzWrapper;
 import com.shatilov.neobuzz.utils.EasyPredictor;
 import com.shatilov.neobuzz.utils.MyoAwareActivity;
 import com.shatilov.neobuzz.utils.MyoWrapper;
 
+import java.io.IOException;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
+
+import fi.iki.elonen.NanoHTTPD;
 
 public class MainActivity extends AppCompatActivity implements MyoAwareActivity, BuzzAwareActivity {
 
     private static final String TAG = "Neo_Buzz_Main_Activity";
+    private static final String ESP_URI = "http://192.168.137.122:5000/";
+    private static final String TEST_STABLE_URI = "http://192.168.137.1:80/";
 
     /* MYO */
     private MyoWrapper myo;
@@ -32,7 +43,13 @@ public class MainActivity extends AppCompatActivity implements MyoAwareActivity,
     /* CLF */
     Queue<float[]> q = new ArrayDeque<>(EasyPredictor.SAMPLES);
     EasyPredictor clf;
+    boolean isClfActive = true;
     String gesture = "00000";
+
+    // TODO remove
+    Button tmpLabel;
+
+    private HandPanel handPanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +57,7 @@ public class MainActivity extends AppCompatActivity implements MyoAwareActivity,
         setContentView(R.layout.activity_main);
 
         clf = new EasyPredictor(this);
+
         initBLEDevices();
         intiUI();
         initComm();
@@ -51,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements MyoAwareActivity,
     }
 
     private void intiUI() {
-        LinearLayout container = findViewById(R.id.hand_container);
+        LinearLayout controlContainer = findViewById(R.id.control_container);
 
         Button myoCB = new Button(this);
         myoCB.setText("Myo");
@@ -62,7 +80,7 @@ public class MainActivity extends AppCompatActivity implements MyoAwareActivity,
                 myo.vibrate();
             }
         });
-        container.addView(myoCB);
+        controlContainer.addView(myoCB);
 
         Button buzzCB = new Button(this);
         buzzCB.setText("Buzz");
@@ -73,22 +91,65 @@ public class MainActivity extends AppCompatActivity implements MyoAwareActivity,
                 buzz.sendVibration();
             }
         });
-        container.addView(buzzCB);
+        controlContainer.addView(buzzCB);
+
+        tmpLabel = new Button(this);
+        controlContainer.addView(tmpLabel);
+
+
+        LinearLayout handContainer = findViewById(R.id.hand_container);
+        handPanel = new HandPanel(this);
+        handContainer.addView(handPanel);
 
     }
 
     private void initComm() {
+
         // HTTP Server
-        Handler serverThread = new Handler();
-        serverThread.post(() -> {
-            AsyncHttpServer server = new AsyncHttpServer();
-            server.get("/", (request, response) -> response.send(gesture));
-            server.listen(5000);
+        Thread serverThread = new Thread(() -> {
+            NanoHTTPD server = new NanoHTTPD(5000) {
+                @Override
+                public Response serve(IHTTPSession session) {
+                    return newFixedLengthResponse(gesture);
+                }
+            };
+            try {
+                server.start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+            } catch (IOException e) {
+                Log.d(TAG, "initComm: Failed to start the server");
+            }
         });
+        serverThread.start();
+
+        // HTTP Client
+        Thread clientThread = new Thread(() -> {
+            RequestQueue queue = Volley.newRequestQueue(this);
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, ESP_URI,
+                    response -> {
+                        if (null != response && !response.isEmpty()) {
+                            tmpLabel.setText(response);
+                        }
+                    },
+                    error -> {
+                        Log.d(TAG, "Failed to connect " + error.getMessage());
+                    });
+
+            boolean keepOn = true;
+            while (keepOn) {
+                try {
+                    queue.add(stringRequest);
+                    Thread.sleep(200);
+                } catch (InterruptedException whatever) {
+                    keepOn = false;
+                }
+            }
+        });
+        clientThread.start();
     }
 
     @Override
     public void onMyoConnect() {
+        Toast.makeText(getApplicationContext(), "Myo is connected", Toast.LENGTH_SHORT).show();
         isMyoConnected = true;
     }
 
@@ -97,15 +158,26 @@ public class MainActivity extends AppCompatActivity implements MyoAwareActivity,
         q.add(emgData);
         if (q.size() == EasyPredictor.SAMPLES) {
             gesture = clf.predict(q);
+
+
             for (int i = 0; i < EasyPredictor.SAMPLES / 2; ++i) {
                 q.poll();
             }
+
+            float[] pos = new float[5];
+            for (int i = 0; i < 5; i++) {
+                pos[i] = Float.parseFloat(String.valueOf(gesture.charAt(i)));
+            }
+            handPanel.setPos(pos);
+
+
         }
     }
 
     @Override
     public void onBuzzConnect(int code) {
         if (code >= 0) {
+            Toast.makeText(getApplicationContext(), "Buzz is connected", Toast.LENGTH_SHORT).show();
             isBuzzConnected = true;
         }
     }
